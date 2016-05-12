8ca87ec3e78f6274971c9813b8587ea9d121b143
Hagen Sommerkorn <hagen.sommerkorn@gmail.com>
2016-05-13 01:28:49 +0200

    refactored require calls to es6 imports

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,30 +1,37 @@
 'use strict';
 
-var _util = require('../shared/util');
-
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _socket = require('socket.io');
+
+var _socket2 = _interopRequireDefault(_socket);
 
 var _compression = require('compression');
 
 var _compression2 = _interopRequireDefault(_compression);
 
+var _util = require('../shared/util');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var server = (0, _express2.default)();
-var http = require('http').Server(server);
-var io = require('socket.io')(http);
+var app = (0, _express2.default)();
+var server = _http2.default.Server(app);
+var io = new _socket2.default(server);
 var port = process.env.PORT || 3000;
 var users = [];
 var sockets = {};
 
-server.use((0, _compression2.default)({}));
-server.use(_express2.default['static'](__dirname + '/../client'));
+app.use((0, _compression2.default)({}));
+app.use(_express2.default['static'](__dirname + '/../client'));
 
 io.on('connection', function (socket) {
     var nick = socket.handshake.query.nick;
-
     var currentUser = {
         id: socket.id,
         nick: nick
@@ -39,9 +46,7 @@ io.on('connection', function (socket) {
         console.log('[INFO] User ' + currentUser.nick + ' connected!');
         sockets[currentUser.id] = socket;
         users.push(currentUser);
-
         io.emit('userJoin', { nick: currentUser.nick });
-
         console.log('[INFO] Total users: ' + users.length);
     }
 
@@ -52,7 +57,6 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         if ((0, _util.findIndex)(users, currentUser.id) > -1) users.splice((0, _util.findIndex)(users, currentUser.id), 1);
         console.log('[INFO] User ' + currentUser.nick + ' disconnected!');
-
         socket.broadcast.emit('userDisconnect', { nick: currentUser.nick });
     });
 
@@ -67,6 +71,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(port, function () {
+server.listen(port, function () {
     console.log('[INFO] Listening on *:' + port);
 });
--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,23 @@
 'use strict';
 
-import {validNick, findIndex, sanitizeString} from '../shared/util';
 import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
 import compression from 'compression';
+import {validNick, findIndex, sanitizeString} from '../shared/util';
 
-let server = express();
-let http = require('http').Server(server);
-let io = require('socket.io')(http);
+let app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
 let port = process.env.PORT || 3000;
 let users = [];
 let sockets = {};
 
-server.use( compression( {} ) );
-server.use(express['static'](__dirname + '/../client'));
+app.use(compression({}));
+app.use(express['static'](__dirname + '/../client'));
 
 io.on('connection', (socket) => {
     let nick = socket.handshake.query.nick;
-
     let currentUser = {
         id: socket.id,
         nick: nick
@@ -31,9 +32,7 @@ io.on('connection', (socket) => {
         console.log('[INFO] User ' + currentUser.nick + ' connected!');
         sockets[currentUser.id] = socket;
         users.push(currentUser);
-
         io.emit('userJoin', {nick: currentUser.nick});
-
         console.log('[INFO] Total users: ' + users.length);
     }
 
@@ -44,7 +43,6 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         if (findIndex(users, currentUser.id) > -1) users.splice(findIndex(users, currentUser.id), 1);
         console.log('[INFO] User ' + currentUser.nick + ' disconnected!');
-
         socket.broadcast.emit('userDisconnect', {nick: currentUser.nick});
     });
 
@@ -53,12 +51,12 @@ io.on('connection', (socket) => {
         let _message = sanitizeString(data.message);
         let date = new Date();
         let time = ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2);
-        
+
         console.log('[CHAT] [' + time + '] ' + _nick + ': ' + _message);
         socket.broadcast.emit('serverSendUserChat', {nick: _nick, message: _message});
     });
 });
 
-http.listen(port, () => {
+server.listen(port, () => {
     console.log('[INFO] Listening on *:' + port);
 });
